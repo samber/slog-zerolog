d2b101d0d01863bc29fbace7992366f2dca9716e
Sebastian Rühl <sruehl@apache.org>
2025-10-24 15:30:07 +0200

    feat: add ZeroLogLeveler

--- zerolog.go
+++ zerolog.go
@@ -4,6 +4,7 @@ import (
 	"log/slog"
 
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 var LogLevels = map[slog.Level]zerolog.Level{
@@ -11,4 +12,26 @@ var LogLevels = map[slog.Level]zerolog.Level{
 	slog.LevelInfo:  zerolog.InfoLevel,
 	slog.LevelWarn:  zerolog.WarnLevel,
 	slog.LevelError: zerolog.ErrorLevel,
+}
+
+var reverseLogLevels map[zerolog.Level]slog.Level
+
+func init() {
+	reverseLogLevels = map[zerolog.Level]slog.Level{}
+	for level, z := range LogLevels {
+		reverseLogLevels[z] = level
+	}
+}
+
+// ZeroLogLeveler can be used for Option.Level (implements slog.Leveler).
+type ZeroLogLeveler struct {
+}
+
+func (ZeroLogLeveler) Level() slog.Level {
+	zeroLogLevel := log.Logger.GetLevel()
+	level, ok := reverseLogLevels[zeroLogLevel]
+	if !ok {
+		return slog.LevelDebug
+	}
+	return level
 }
